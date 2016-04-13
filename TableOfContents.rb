cf16741b4686873f867cb71423fed1fe0d9ed5cb
alexanderwjrussell <awjrussell@live.co.uk>
2016-04-13 17:36:01 +0100

    Fixed table of contents syntax error

--- TableOfContents.rb
+++ TableOfContents.rb
@@ -1,4 +1,4 @@
-lineWidth = 40
+lineWidth = 80
 title = 'Table of Contents'
 line1_left = 'Chapter 1: Numbers'
 line1_right = 'page 1'
@@ -7,9 +7,10 @@ line2_right = 'page 72'
 line3_left = 'Chapter 3: Variables'
 line3_right = 'page 118'
 
-line1 = line1_left.ljust (lineWidth/2) + line1_right.rjust (lineWidth/2)
-line2 = line2_left.ljust (lineWidth/2) + line2_right.rjust (lineWidth/2)
-line3 = line3_left.ljust (lineWidth/2) + line3_right.rjust (lineWidth/2)
+title = title.center(lineWidth)
+line1 = line1_left.ljust(lineWidth/2) + line1_right.rjust(lineWidth/2)
+line2 = line2_left.ljust(lineWidth/2) + line2_right.rjust(lineWidth/2)
+line3 = line3_left.ljust(lineWidth/2) + line3_right.rjust(lineWidth/2)
 
 tableOfContents = [title, line1, line2, line3]
 
